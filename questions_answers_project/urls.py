451a8c38ed0c579266965a6304ed3995127a843c
dmitriy <dmlegasy@gmail.com>
2025-11-12 22:01:40 +0300

    Add urls for interactive docs

--- questions_answers_project/urls.py
+++ questions_answers_project/urls.py
@@ -14,11 +14,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include("questions_answers_app.urls", namespace="questions_answers_app")),
+    path("admin/", admin.site.urls),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/schema/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "service/",
+        include("questions_answers_app.urls", namespace="questions_answers_app"),
+    ),
 ]
