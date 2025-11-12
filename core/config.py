27e4038b2d0ab49bb63e515cbf7e1b13396e898e
dmitriy <dmlegasy@gmail.com>
2025-11-12 14:46:15 +0300

    Add congig for sensetive data

---
+++ core/config.py
@@ -0,0 +1,14 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    django_debug: bool
+    django_secret_key: str
+
+    db_name: str
+    db_user: str
+    db_password: str
+    db_host: str
+    db_port: int
+
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
