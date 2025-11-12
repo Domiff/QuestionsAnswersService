a91593e496ab5bac80e95f23c5ba804266692cd0
dmitriy <dmlegasy@gmail.com>
2025-11-12 15:32:52 +0300

    Add settings for drf and drf_spectacular

--- questions_answers_project/settings.py
+++ questions_answers_project/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
+    'drf_spectacular',
+
     'questions_answers_app.apps.QuestionsAnswersAppConfig',
 ]
 
@@ -129,3 +132,17 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#DRF
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Questions Answers API',
+    'DESCRIPTION': 'Questions Answers API Service',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+}
