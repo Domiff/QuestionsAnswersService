5420edf91087e0168cf17f1ab68b89d223f480e1
dmitriy <dmlegasy@gmail.com>
2025-11-12 22:08:58 +0300

    Add urls for app

---
+++ questions_answers_app/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+
+from .views import (
+    GetAllQuestion,
+    CreateQuestion,
+    GetOneQuestion,
+    DeleteQuestion,
+    GetAnswer,
+    CreateAnswer,
+    DeleteAnswer,
+)
+
+app_name = "questions_answers_app"
+
+
+urlpatterns = [
+    path("questions/", GetAllQuestion.as_view(), name="questions"),
+    path("questions/create", CreateQuestion.as_view(), name="create_question"),
+    path("questions/<int:id>", GetOneQuestion.as_view(), name="question"),
+    path("questions/delete/<int:id>", DeleteQuestion.as_view(), name="delete_question"),
+    path("questions/<int:id>/answers", CreateAnswer.as_view(), name="create_answer"),
+    path("answers/<int:id>", GetAnswer.as_view(), name="get_answers"),
+    path("answers/delete/<int:id>", DeleteAnswer.as_view(), name="delete_answers"),
+]
