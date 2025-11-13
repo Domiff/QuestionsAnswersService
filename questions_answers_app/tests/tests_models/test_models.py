cefbb6ee88c482d6a643a62bf079aff22c1c8ee6
dmitriy <dmlegasy@gmail.com>
2025-11-13 20:36:35 +0300

    Create tests models

---
+++ questions_answers_app/tests/tests_models/test_models.py
@@ -0,0 +1,14 @@
+import pytest
+
+
+@pytest.mark.django_db
+def test_question(question_factory):
+    question = question_factory()
+    assert isinstance(question.text, str)
+
+
+@pytest.mark.django_db
+def test_answer(answer_factory):
+    answer = answer_factory()
+    assert isinstance(answer.text, str)
+    assert isinstance(answer.user_id , str)
