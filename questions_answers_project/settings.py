c97e4c1378877a85b818e75ea1390e421126e4d0
dmitriy <dmlegasy@gmail.com>
2025-11-13 21:49:24 +0300

    Add ALLOWED_HOSTS

--- questions_answers_project/settings.py
+++ questions_answers_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = settings.django_secret_key
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = settings.django_debug
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "0.0.0.0"]
 
 
 # Application definition
