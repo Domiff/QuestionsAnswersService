dce21b7db1f3149bff0038b1ee5c2a70c5968af4
Dmitriy <158328298+Domiff@users.noreply.github.com>
2025-11-28 18:14:19 +0300

    Add logging configuration for GitHub Actions

--- questions_answers_project/settings.py
+++ questions_answers_project/settings.py
@@ -172,3 +172,18 @@ LOGGING = {
         "level": "INFO",
     },
 }
+
+if os.getenv("GITHUB_ACTIONS"):
+    LOGGING = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+            },
+        },
+        "root": {
+            "handlers": ["console"],
+            "level": "INFO",
+        },
+    }
